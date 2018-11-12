5b69b2b2709b7859dcf1a4df2a112b59382ee637
Lorna Wilson <lorna.i.wilson@gmail.com>
2018-11-12 09:44:07 -0900

    Update code for new data files with 4 sig figs in circulus measurements and for analysis of the sum of circulus measurements per zone, not of individual circulus measurements

--- circ.ssm.R
+++ circ.ssm.R
@@ -3,11 +3,12 @@
 #date: "Started September 12, 2018"
 
 #Read circulus data into workspace
-pl.circ = read.csv(file="CirclusSP.txt", header = TRUE, sep = ",")
-names(pl.circ)
+pl.circ = read.csv(file="SP.4.csv", header = TRUE, sep = ",")
+s4.circ = read.csv(file="S4.4.csv", header = TRUE, sep = ",")
+s3.circ = read.csv(file="S3.4.csv", header = TRUE, sep = ",")
+names(s3.circ)==names(s4.circ)
+names(s4.circ)==names(pl.circ)
 
-s4.circ = read.csv(file="S4circuli.txt", header = TRUE, sep = ",")
-names(pl.circ)
 
 ############Read SST temp data into workspace
 sst.area.sum = read.csv(file="SSTbyAreaSum.csv", header = TRUE, sep = ",")
@@ -20,16 +21,19 @@ head(pop.season.area)
 
 ###########################################################
 #Make specimen table
-fish.spec = cbind.data.frame(pl.circ$image.name, pl.circ$sys, pl.circ$year, pl.circ$project.code, pl.circ$doy, pl.circ$fishlength)
-dim(fish.spec)
+fish.spec = cbind.data.frame(s3.circ$image.name, s3.circ$sys, s3.circ$year, s3.circ$project.code, s3.circ$doy, s3.circ$fishlength)
 fish.spec = unique(fish.spec)
 dim(fish.spec)
+head(fish.spec)
 #whew
 names(fish.spec)=c("image.name", "sys", "year", "project.code", "doy", "fish.length")
 #Maturity age from image name:
 fish.spec$m.age = substr(fish.spec$image.name, 5,6)
 #limit to only 1.3 and 1.4 ages
+head(fish.spec$m.age)
+unique(fish.spec$m.age)
 fish.spec = fish.spec[fish.spec$m.age %in% c(14, 13),]
+unique(fish.spec$sys)
 #NEED TO SEPARATE KENAI RUNS
 
 #generate circulus responses of summary stats by fish
@@ -37,13 +41,33 @@ library(moments)
 library(dplyr)
 pl.circ.resp = pl.circ %>% 
   group_by(image.name) %>% 
-  summarise(n = n(), Mean = mean(meas), Median = (median(meas)),FiveP = quantile(meas,.05), NinetyfiveP = quantile(meas,.95), SD = sd(meas), Skew = skewness(meas), kurt = kurtosis(meas))
+  summarise(Pl = sum(meas))
+
+s4.circ.resp = s4.circ %>% 
+  group_by(image.name) %>% 
+  summarise(S4 = sum(meas))
+
+s3.circ.resp = s3.circ %>% 
+  group_by(image.name) %>% 
+  summarise(S3 = sum(meas))
+
+
 
 #need response and grouping variables in same dataframe
-fish.spec = merge(pl.circ.resp, fish.spec, by = "image.name")
-fish.spec$by = fish.spec$year - (as.numeric(substr(fish.spec$image.name, 5,5))+as.numeric(substr(fish.spec$image.name, 6,6)))
+fish.spec = merge(s3.circ.resp, fish.spec, by = "image.name", all=T)
+fish.spec = merge(pl.circ.resp, fish.spec, by = "image.name", all=T)
+fish.spec = merge(s4.circ.resp, fish.spec, by = "image.name", all=T)
 
 head(fish.spec)
+
+fish.spec$by = fish.spec$year - (as.numeric(substr(fish.spec$image.name, 5,5))+as.numeric(substr(fish.spec$image.name, 6,6)))
+
+summary(fish.spec)
+
+#s3.circ.resp = s4.circ %>% 
+#  group_by(image.name) %>% 
+#  summarise(n = n(), Mean = mean(meas), Median = (median(meas)),FiveP = quantile(meas,.05), NinetyfiveP = quantile(meas,.95), SD = sd(meas), Skew = skewness(meas), kurt = kurtosis(meas))
+
 
 #######Add SST to dataframe
 #Merge area to pop
@@ -72,58 +96,48 @@ fish.spec$Year = NULL
 
 head(fish.spec)
 
+cor.test(fish.spec$sum,fish.spec$wint)
 
 ######################################3
 #predict fish length
 library(lme4)
-lme.pl1 = lmer(fish.length ~  as.factor(m.age) + (1|year) + sum + wint +(1|sys) + (1|by), data = fish.spec) 
 lme.pl2 = lmer(fish.length ~  as.factor(m.age) + (1|year)  + wint +(1|sys) + (1|by), data = fish.spec) 
 lme.pl3 = lmer(fish.length ~  as.factor(m.age) + (1|year)  + sum +(1|sys) + (1|by), data = fish.spec) 
 lme.pl4 = lmer(fish.length ~  as.factor(m.age) + (1|year) +(1|sys) + (1|by), data = fish.spec) 
 library(AICcmodavg)
-
-models<-list(lme.pl1, lme.pl2, lme.pl3, lme.pl4)
-Modnames <- c('lme.pl1','lme.pl2','lme.pl3','lme.pl4')
+models<-list(lme.pl2, lme.pl3, lme.pl4)
+Modnames <- c('lme.pl2','lme.pl3','lme.pl4')
 aictab(cand.set = models, modnames = Modnames, sort = TRUE)
-anova(lme.pl1, lme.pl3)
+anova(lme.pl3, lme.pl2)
+
+acf(resid(lme.pl2))
+pacf(resid(lme.pl2)) #lag 8? 
+library(forecast)
+lme.pl2.ac = auto.arima(resid(lme.pl2)) #ARIMA(5,0,5) with zero mean; M5b.ac
+lme.pl2.ac #Ar1 coeff = 0.0141, SE 0.0085
+summary(lme.pl2)
 
 
 #Plot circ data
 library(ggplot2)
 library(ggridges) 
 names(fish.spec)
-p1 = ggplot(fish.spec, aes(x = Mean, y = sys, group = sys, fill=factor(..quantile..))) +
+p1 = ggplot(fish.spec, aes(x =Pl, y = sys, group = sys, fill=factor(..quantile..))) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
   scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),
                     labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p1
-p1 = ggplot(fish.spec, aes(x = Mean, y = year, group = year, fill=factor(..quantile..))) +
+p1 = ggplot(fish.spec, aes(x = Pl, y = year, group = year, fill=factor(..quantile..))) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
   scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),
                     labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p1
-#no obvious trends
 
-p2 = ggplot(fish.spec, aes(x = Skew, y = sys, group = sys, fill=factor(..quantile..))) +
-  stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
-  scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),
-                    labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p2
-#Good that plots look somewhat similar among stocks
-p2 = ggplot(fish.spec, aes(x = Skew, y = year, group = year, fill=factor(..quantile..))) +
-  stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
-  scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),                    labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p2
-# hm, skewness in 1980 and previous are inconsistent
 
-p2 = ggplot(fish.spec, aes(x = SD, y = year, group = year, fill=factor(..quantile..))) +
-  stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
-  scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),                    labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p2
 
-p2 = ggplot(fish.spec, aes(x = kurt, y = year, group = year, fill=factor(..quantile..))) +
-  stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
-  scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),                    labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p2
 
 
 #So, look at lengths by stock over time
 p2 = ggplot(fish.spec, aes(year, fish.length, group = year)) + geom_boxplot(aes(fill = m.age)); p2
-p2 + facet_grid(m.age~sys) + theme_bw() + ylim(425, 1200)
+p2 + facet_wrap(m.age~sys) + theme_bw() + ylim(425, 1200)
 
 
 names(fish.spec)
