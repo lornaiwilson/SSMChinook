5ddf7f3e6a6aa955cbadf8fed45de3f246ec0189
Lorna Wilson <lorna.i.wilson@gmail.com>
2018-11-12 22:29:22 -0900

    Analysis re-run with cumulative circulus measurements

--- circ.ssm.R
+++ circ.ssm.R
@@ -1,146 +1,129 @@
 #title: "Data Wrangling: Case Study circuli SSM"
 #author: "Lorna I Wilson"
-#date: "Started September 12, 2018"
+#date: Started September 12, 2018
 
-#Read circulus data into workspace
-pl.circ = read.csv(file="SP.4.csv", header = TRUE, sep = ",")
-s4.circ = read.csv(file="S4.4.csv", header = TRUE, sep = ",")
-s3.circ = read.csv(file="S3.4.csv", header = TRUE, sep = ",")
-names(s3.circ)==names(s4.circ)
-names(s4.circ)==names(pl.circ)
+circ = read.csv(file="circuli.csv", header = TRUE, sep = ",")
+names(circ)
 
+circ = circ[circ$AdultAge %in% c(14, 13),]
+circ = circ[circ$DayofYear > 100,]
+circ = na.omit(circ, cols="AdultLength")
 
-############Read SST temp data into workspace
-sst.area.sum = read.csv(file="SSTbyAreaSum.csv", header = TRUE, sep = ",")
-sst.area.wint = read.csv(file="SSTbyAreaWint.csv", header = TRUE, sep = ",")
-head(sst.area.wint)
-
-pop.season.area = read.csv(file="SysAreas.csv", header = TRUE, sep = ",")
-head(pop.season.area)
-
-
-###########################################################
-#Make specimen table
-fish.spec = cbind.data.frame(s3.circ$image.name, s3.circ$sys, s3.circ$year, s3.circ$project.code, s3.circ$doy, s3.circ$fishlength)
-fish.spec = unique(fish.spec)
-dim(fish.spec)
-head(fish.spec)
-#whew
-names(fish.spec)=c("image.name", "sys", "year", "project.code", "doy", "fish.length")
-#Maturity age from image name:
-fish.spec$m.age = substr(fish.spec$image.name, 5,6)
-#limit to only 1.3 and 1.4 ages
-head(fish.spec$m.age)
-unique(fish.spec$m.age)
-fish.spec = fish.spec[fish.spec$m.age %in% c(14, 13),]
-unique(fish.spec$sys)
-#NEED TO SEPARATE KENAI RUNS
-
-#generate circulus responses of summary stats by fish
-library(moments)
-library(dplyr)
-pl.circ.resp = pl.circ %>% 
-  group_by(image.name) %>% 
-  summarise(Pl = sum(meas))
-
-s4.circ.resp = s4.circ %>% 
-  group_by(image.name) %>% 
-  summarise(S4 = sum(meas))
-
-s3.circ.resp = s3.circ %>% 
-  group_by(image.name) %>% 
-  summarise(S3 = sum(meas))
+summary(circ$AdultLength)
 
 
 
-#need response and grouping variables in same dataframe
-fish.spec = merge(s3.circ.resp, fish.spec, by = "image.name", all=T)
-fish.spec = merge(pl.circ.resp, fish.spec, by = "image.name", all=T)
-fish.spec = merge(s4.circ.resp, fish.spec, by = "image.name", all=T)
-
-head(fish.spec)
-
-fish.spec$by = fish.spec$year - (as.numeric(substr(fish.spec$image.name, 5,5))+as.numeric(substr(fish.spec$image.name, 6,6)))
-
-summary(fish.spec)
-
-#s3.circ.resp = s4.circ %>% 
-#  group_by(image.name) %>% 
-#  summarise(n = n(), Mean = mean(meas), Median = (median(meas)),FiveP = quantile(meas,.05), NinetyfiveP = quantile(meas,.95), SD = sd(meas), Skew = skewness(meas), kurt = kurtosis(meas))
-
-
-#######Add SST to dataframe
-#Merge area to pop
-fish.spec = merge(fish.spec, pop.season.area, by.x = "sys", by.y = "Pop")
-###Merge temps to area and years
-#make common fields
-sst.area.sum$YrArea = paste(sst.area.sum$Year, sst.area.sum$Area)
-sst.area.wint$YrArea = paste(sst.area.wint$Year, sst.area.wint$Area)
-
-fish.spec$YrSumArea = paste(fish.spec$year, fish.spec$Sum.Area)
-fish.spec$YrWintArea = paste(fish.spec$year, fish.spec$Wint.area)
-
-#Merge sum temps in
-fish.spec = merge(fish.spec, sst.area.sum, by.x = "YrSumArea", by.y = "YrArea")
-#remove extra columns from merge, Area and Year
-fish.spec$Area = NULL
-fish.spec$Year = NULL
-
-head(fish.spec)
-
-#Merge wint temps in
-fish.spec = merge(fish.spec, sst.area.wint, by.x = "YrWintArea", by.y = "YrArea")
-#remove extra columns from merge, Area and Year
-fish.spec$Area = NULL
-fish.spec$Year = NULL
-
-head(fish.spec)
-
-cor.test(fish.spec$sum,fish.spec$wint)
-
-######################################3
+######################################3#######################
 #predict fish length
 library(lme4)
-lme.pl2 = lmer(fish.length ~  as.factor(m.age) + (1|year)  + wint +(1|sys) + (1|by), data = fish.spec) 
-lme.pl3 = lmer(fish.length ~  as.factor(m.age) + (1|year)  + sum +(1|sys) + (1|by), data = fish.spec) 
-lme.pl4 = lmer(fish.length ~  as.factor(m.age) + (1|year) +(1|sys) + (1|by), data = fish.spec) 
+lme.pl1 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY), data = circ) 
+lme.pl2 = lmer(AdultLength ~  as.factor(AdultAge)+(1|sys)+ (1|BY), data = circ) 
+lme.pl3 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+ (1|BY), data = circ) 
+lme.pl4 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys), data = circ) 
+lme.pl5 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year), data = circ) 
+lme.pl6 = lmer(AdultLength ~  as.factor(AdultAge) +(1|sys), data = circ) 
+lme.pl7 = lmer(AdultLength ~  as.factor(AdultAge) +(1|BY), data = circ) 
+
 library(AICcmodavg)
-models<-list(lme.pl2, lme.pl3, lme.pl4)
-Modnames <- c('lme.pl2','lme.pl3','lme.pl4')
+models<-list(lme.pl1,lme.pl2, lme.pl3, lme.pl4, lme.pl5, lme.pl6, lme.pl7)
+Modnames <- c('lme.pl1','lme.pl2','lme.pl3','lme.pl4','lme.pl5','lme.pl6','lme.pl7')
 aictab(cand.set = models, modnames = Modnames, sort = TRUE)
-anova(lme.pl3, lme.pl2)
+#lme.pl1
+lme.pl1 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY), data = circ, REML = FALSE)
+lme.pl8 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY)+
+                 as.numeric(LA.sum), data = circ, REML = FALSE)
+lme.pl9 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY)+
+                 as.numeric(LA.wint), data = circ, REML = FALSE) 
+lme.pl10 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY)+
+                 as.numeric(DayofYear), data = circ, REML = FALSE)
+lme.pl11 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY)+
+                 as.numeric(LA.sum)+as.numeric(DayofYear), data = circ, REML = FALSE) 
+lme.pl12 = lmer(AdultLength ~  as.factor(AdultAge)+(1|Year)+(1|sys)+(1|BY)+
+                  as.numeric(LA.wint)+as.numeric(DayofYear), data = circ, REML = FALSE)
 
-acf(resid(lme.pl2))
-pacf(resid(lme.pl2)) #lag 8? 
+models<-list(lme.pl1,lme.pl8,lme.pl8,lme.pl10,lme.pl11,lme.pl12 )
+Modnames <- c('lme.pl1','lme.pl8','lme.pl9','lme.pl10','lme.pl11','lme.pl12')
+aictab(cand.set = models, modnames = Modnames, sort = TRUE)
+
+summary(lme.pl12)
+acf(resid(lme.pl12))
+pacf(resid(lme.pl12)) 
 library(forecast)
-lme.pl2.ac = auto.arima(resid(lme.pl2)) #ARIMA(5,0,5) with zero mean; M5b.ac
-lme.pl2.ac #Ar1 coeff = 0.0141, SE 0.0085
-summary(lme.pl2)
+lme.pl12.ac = auto.arima(resid(lme.pl12)) #ARIMA(1,0,0) with zero mean
+summary(lme.pl12.ac)
+lme.pl12.ac #Ar1 coeff = 0.0914, SE 0.0226
+#ar1      ar2      ma1      ma2
+#1.1723  -0.2012  -0.5476  -0.3523
+#s.e.0.0148   0.0121   0.0143   0.0090
 
+
+library(MuMIn)
+r.squaredGLMM(lme.pl12) #Conditional R2 = 0.57, main effects R2 = 0.23
+length(circ$Name) #32514
+length(resid(lme.pl12)) #32514
+
+circ$adj.len = resid(lme.pl12)
+
+names(circ)
+
+#########################
+#Summary circulus measurement table
+library(dplyr)
+circ.summ = circ %>% 
+  group_by(sys, AdultAge) %>% 
+  summarise(n = n(), First = min(Year), Last = max(Year))
+circ.summ
 
 #Plot circ data
 library(ggplot2)
 library(ggridges) 
-names(fish.spec)
-p1 = ggplot(fish.spec, aes(x =Pl, y = sys, group = sys, fill=factor(..quantile..))) +
+names(circ)
+p1 = ggplot(circ, aes(x=Radius, y = sys, group = sys, fill=factor(..quantile..))) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
   scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),
                     labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p1
-p1 = ggplot(fish.spec, aes(x = Pl, y = year, group = year, fill=factor(..quantile..))) +
+p1 = ggplot(circ, aes(x=Radius, y = Year, group = Year, fill=factor(..quantile..))) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE, quantiles = c(0.05, 0.95)) +
   scale_fill_manual(name = "Quantile", values = c("#E69F00", "#999999", "#56B4E9"),
                     labels = c("(0, 0.05)", "(0.05, 0.95)", "(0.95, 1)")); p1
 
+names(circ)
+#Look at lengths by stock over time
+p2 = ggplot(circ, aes(Year, AdultLength, group = Year, fill = as.factor(AdultAge)))+ geom_boxplot(aes(fill = as.factor(AdultAge)))+
+  facet_wrap(sys~.) + theme_bw() + ylim(625, 1000)
+p2
+
+names(circ)
+head(circ$LastAnn)
+mode(circ$LastAnn)
+
+circ$LastAnn = as.numeric(circ$LastAnn)
+names(circ)
+library(moments)
+last.ann.resp = circ %>% 
+  group_by(Year, sys, AdultAge) %>% 
+  summarise(n = n(), MeanAL = mean(adj.len),MeanAL = mean(adj.len), MedianLA = median(LastAnn),FiveP = quantile(LastAnn,.05), 
+            NinetyfiveP = quantile(LastAnn,.95), SD = sd(LastAnn), kurt = kurtosis(LastAnn),Skew = skewness(LastAnn))
+
+last.ann.resp = as.data.frame(last.ann.resp)
+
+
+head(last.ann.resp)
+plot(last.ann.resp$MedianLA~last.ann.resp$MeanAL)
+
+#Length change over time?
+plot(last.ann.resp$MeanAL~last.ann.resp$Year)
+summary(lm(last.ann.resp$MeanAL~last.ann.resp$Year)) #no relation to Year
+
+summary(lm(last.ann.resp$MeanAL~last.ann.resp$MedianLA))
+#postively related, good, this is unexplained variance
+
+
+summary(lm(last.ann.resp$MeanAL~last.ann.resp$kurt)) #no relation to kurtosis
+plot(last.ann.resp$kurt~last.ann.resp$MeanAL)
+summary(lm(last.ann.resp$MeanAL~last.ann.resp$Skew)) #negatively related to skewness
+plot(last.ann.resp$Skew~last.ann.resp$MeanAL)
 
 
 
-
-#So, look at lengths by stock over time
-p2 = ggplot(fish.spec, aes(year, fish.length, group = year)) + geom_boxplot(aes(fill = m.age)); p2
-p2 + facet_wrap(m.age~sys) + theme_bw() + ylim(425, 1200)
-
-
-names(fish.spec)
-#
-
-
+       
